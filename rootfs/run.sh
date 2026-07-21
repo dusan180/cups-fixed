f15e04c0c871085f325993c7a00ed898f783c25d
root <root@a0d7b954-ssh.local.hass.io>
2026-07-21 23:43:22 +0200

    Add persistent HPLIP plugin storage

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -11,7 +11,11 @@ if [ ! -d /config/cups ]; then cp -v -R /etc/cups /config; fi
 rm -v -fR /etc/cups
 
 ln -v -s /config/cups /etc/cups
-
+bashio::log.info "Preparing HP plugin state"
+mkdir -p /var/lib/hp
+if [ -f /config/hp/hplip.state ]; then
+  cp -f /config/hp/hplip.state /var/lib/hp/hplip.state
+fi
 bashio::log.info "Starting CUPS server as CMD from S6"
 
 cupsd -f
