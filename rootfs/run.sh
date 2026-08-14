6daedf59f7084c5f7ba229b8210f796179925b8c
root <root@a0d7b954-ssh.local.hass.io>
2026-08-14 12:19:23 +0200

    Add automatic HP USB URI watchdog

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/with-contenv bashio
 
 ulimit -n 524288
 
@@ -24,6 +23,26 @@ rm -rf /usr/share/hplip/prnt/plugins
 
 ln -s /config/hplip-prnt-plugins /usr/share/hplip/prnt/plugins
 
+bashio::log.info "Starting HP USB URI watchdog"
+
+(
+  while true; do
+    sleep 30
+
+    detected_uri="$(hp-probe -busb 2>/dev/null | awk '/hp:\/usb\// {print $1; exit}')"
+
+    if [ -z "$detected_uri" ]; then
+      continue
+    fi
+
+    current_uri="$(lpstat -v HP_LaserJet_Professional_P1102 2>/dev/null | sed 's/^device for HP_LaserJet_Professional_P1102: //')"
+
+    if [ -n "$current_uri" ] && [ "$current_uri" != "$detected_uri" ]; then
+      bashio::log.warning "HP printer URI changed: ${current_uri} -> ${detected_uri}"
+      lpadmin -p HP_LaserJet_Professional_P1102 -v "$detected_uri"
+    fi
+  done
+) &
 
 bashio::log.info "Starting CUPS server as CMD from S6"
 
